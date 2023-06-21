d640d9ed7c0512e266f99a508aeb9158509ae010
High23 <warenasb2@gmail.com>
2023-06-21 01:35:26 -0700

    Improved password check

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-createAccountButton = document.querySelector('#createAccount');
-createAccountButton.addEventListener('click', () => {
+let password = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirmPassword');
+
+
+password.addEventListener("change", () => {
     password = document.querySelector('#password');
     confirmPassword = document.querySelector('#confirmPassword');
-    if (!(password.value === confirmPassword.value)){
-        password.classList.add('error');
-        confirmPassword.classList.add('error');
-    } else {
-        password.classList.remove('error');
-        confirmPassword.classList.remove('error');
-    }
+    checkPasswords(password, confirmPassword);
 });
 
+confirmPassword.addEventListener("change", () => {
+    password = document.querySelector('#password');
+    confirmPassword = document.querySelector('#confirmPassword');
+    checkPasswords(password, confirmPassword);
+});
+
+function checkPasswords(passwordOne, passwordTwo){
+    if (passwordOne.value === "" && passwordTwo === ""){
+        passwordOne.classList.remove('good');
+        passwordTwo.classList.remove('good');
+        passwordOne.classList.remove('error');
+        passwordTwo.classList.remove('error');
+    } 
+    else if (!(passwordOne.value === passwordTwo.value)){
+        passwordOne.classList.add('error');
+        passwordTwo.classList.add('error');
+        passwordOne.classList.remove('good');
+        passwordTwo.classList.remove('good');
+    } 
+    else {
+        passwordOne.classList.remove('error');
+        passwordTwo.classList.remove('error');
+        passwordOne.classList.add('good');
+        passwordTwo.classList.add('good');
+    }
+}
